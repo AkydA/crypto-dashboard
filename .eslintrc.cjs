9dab217de143939d471d7530b49fce14a8c1c10e
AkydA <psw9704@gmail.com>
2024-05-04 11:52:36 +0900

    initialize project

---
+++ .eslintrc.cjs
@@ -0,0 +1,69 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  plugins: ["react", "react-refresh", "@typescript-eslint", "css-import-order"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:css-import-order/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling", "index"],
+          "unknown",
+        ],
+        pathGroups: [
+          {
+            pattern: "~/**/*",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "*.{svg,png,jpg}",
+            patternOptions: {
+              dot: true,
+              nocomment: true,
+              matchBase: true,
+            },
+            group: "unknown",
+            position: "before",
+          },
+          {
+            pattern: "*.{scss,css}",
+            patternOptions: {
+              dot: true,
+              nocomment: true,
+              matchBase: true,
+            },
+            group: "unknown",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react", "unknown"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        warnOnUnassignedImports: true,
+      },
+    ],
+  },
+};
